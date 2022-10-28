ddb5f9f91ebba73ffbb64bf0409e37a1c0365cf0
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-10-29 01:51:14 +0800

    Add member in checkout page

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
-    $('.xzoom, .xzoom-gallery').xzoom({
-        zoomWidth: 500,
-        title: false,
-        tint: '#333',
-        xoffset: 15,
-    })
+    const xzoom  = $('.xzoom, .xzoom-gallery')
+    const datepicker = $('.datepicker')
+
+    if(xzoom.length > 0){
+        xzoom.xzoom({
+            zoomWidth: 500,
+            title: false,
+            tint: '#333',
+            xoffset: 15,
+        })
+    }
+
+    if(datepicker.length > 0){
+        datepicker.datepicker({
+            showOn: "both",
+            buttonImage: "images/icon/ic_doe.svg",
+            buttonImageOnly: true,
+            dateFormat: 'yy-mm-dd'
+        })
+    }
 })
