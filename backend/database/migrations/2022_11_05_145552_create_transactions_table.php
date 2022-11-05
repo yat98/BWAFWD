1c2aad794b362ece1e7206787fc8a393ed8208c7
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-05 22:59:33 +0800

    Add trasactions migration

---
+++ backend/database/migrations/2022_11_05_145552_create_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('travel_package_id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->integer('additional_visa');
+            $table->unsignedBigInteger('transaction_total');
+            /**
+             * Status
+             * in_card, pending, success, cancel, failed
+             */
+            $table->string('transaction_status');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
