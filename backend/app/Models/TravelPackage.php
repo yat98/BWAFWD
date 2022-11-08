8599a92a6ff5af2e16be473912125948d24e3144
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-09 00:07:28 +0800

    Add TravelPackage model

---
+++ backend/app/Models/TravelPackage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class TravelPackage extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'location',
+        'about',
+        'featured_event',
+        'languange',
+        'food',
+        'departure',
+        'duration',
+        'type',
+        'price',
+    ];
+
+    protected $hidden = [];
+}
