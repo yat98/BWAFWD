449766cc33a5efc697a7f8b7b90cf5a0bb1ceebe
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-10 22:53:50 +0800

    Add transaction member model

---
+++ backend/app/Models/TransactionMember.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class TransactionMember extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'transaction_id',
+        'username',
+        'nationality',
+        'is_visa',
+        'doe_passport',
+    ];
+
+    public function transaction(){
+        return $this->belongsTo(Transaction::class, 'transaction_id');
+    }
+}
