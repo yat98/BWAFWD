2615534b66563817a49bc9e927b8d14bcb9dbbf5
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-10 22:44:32 +0800

    Add transaction model

---
+++ backend/app/Models/Transaction.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Transaction extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'travel_package_id',
+        'user_id',
+        'additional_visa',
+        'transaction_total',
+        'transaction_status',
+    ];
+
+    protected $hidden = [];
+
+    public function travelPackage(){
+        return $this->belongsTo(travelPackage::class, 'travel_package_id');
+    }
+
+    public function transactionMembers(){
+        return $this->hasMany(TransactionMember::class, 'transaction_id');
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
