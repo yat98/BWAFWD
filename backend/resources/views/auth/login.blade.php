1c6515f6262c92dd75dc272c62b247d971e3998a
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-12 19:51:23 +0800

    Add slicing sign in page in frontend & backend

--- backend/resources/views/auth/login.blade.php
+++ backend/resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app>
+{{-- <x-layouts.app>
     <x-slot:title>Login</x-slot:title>
     <div class="container mt-5">
         <div class="row justify-content-center">
@@ -69,4 +69,78 @@
             </div>
         </div>
     </div>
-</x-layouts.app>
+</x-layouts.app> --}}
+
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nomads &mdash; Sign In</title>
+    <x-styles.style />
+</head>
+<body>
+    <!-- Main -->
+    <main class="login-container">
+        <div class="container">
+            <div class="row page-login d-flex align-items-center">
+                <div class="section-left col-12 col-md-6">
+                    <h1 class="mb-4 d-none d-md-block d-lg-block">
+                        We explore the new 
+                        <br>
+                        life much better
+                    </h1>
+                    <img src="{{ asset('frontend/img/sign_in/img.jpg') }}" class="w-75 d-none d-md-block d-lg-block">
+                </div>
+                <div class="section-left col-12 col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="text-center mt-3">
+                                <img src="{{ asset('frontend/img/logo/logo.svg') }}" alt="Logo NOMADS">
+                            </div>
+                            <form method="POST" action="{{ route('login') }}" class="mt-5">
+                                @csrf
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email address</label>
+                                    <input type="email" name="email" class="form-control  @error('email') is-invalid @enderror" id="email">
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Password</label>
+                                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password">
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="mb-3 form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                                <button type="submit" class="btn btn-login w-100 mt-4">Sign In</button>
+                                <p class="text-center mt-4">
+                                    <a href="{{ route('password.request') }}" class="text-muted">Forget Password</a>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <script src="libraries/jQuery/jquery-3.6.1.min.js"></script>
+    <script src="libraries/bootstrap/js/bootstrap.min.js"></script>
+    <script src="libraries/retina/retina.min.js"></script>
+    <script src="scripts/main.js"></script>
+</body>
+</html>
